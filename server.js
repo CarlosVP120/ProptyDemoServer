79a3e0770d2d864b7d4f0d17db75f0c6a66d11b2
CarlosVP120 <carlosveryan4@gmail.com>
2023-07-31 22:49:22 -0600

    Pings

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 let cron = require("node-cron");
+const https = require("https");
 const port = 3000;
 
 cron.schedule("*/10 * * * *", () => {
   // Ping to "https://propty-file-server.onrender.com" to keep it awake
   fetch("https://propty-file-server.onrender.com");
+  https.get("https://propty-file-server.onrender.com");
   console.log("Ping to https://propty-file-server.onrender.com");
 
   // Ping to "https://flowise-7ne0.onrender.com" to keep it awake
   fetch("https://flowise-7ne0.onrender.com");
+  https.get("https://flowise-7ne0.onrender.com");
   console.log("Ping to https://flowise-7ne0.onrender.com");
 });
 
